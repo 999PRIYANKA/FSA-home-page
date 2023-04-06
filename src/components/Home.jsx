7fd5479284fe9c459ee9cc5aa84a8dcb9e71b60e
Ayaansh Rajotia <86037577+ayaanshrajotia@users.noreply.github.com>
2023-04-06 22:59:33 +0530

    fixed the slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,34 +14,56 @@ import { FaGraduationCap } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 
 const settings = {
-  dots: true,
+  dots: false,
+  arrows: false,
   infinite: true,
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
 };
 
 const Home = () => {
   return (
     <div className="home">
-        <Slide
-          title="Online Campus preparation portal with practice sheets"
-          desc="It's 2023. Deliver the learning experience your team deserves. And
+      <div className="home-main">
+        <Socialbar />
+        <Slider {...settings}>
+          <Slide
+            title="Campus recruitment training"
+            desc="It's 2023. Deliver the learning experience your team deserves. And
             have a good time doing it."
-          img="https://as2.ftcdn.net/v2/jpg/04/41/98/07/1000_F_441980769_s5CbR4NGlv9aT9MbacDGvlpLAFbynfya.jpg"
-        />
-        {/* <Slide
-          title="Online Campus preparation portal with practice sheets"
-          desc="It's 2023. Deliver the learning experience your team deserves. And
+            img="https://as2.ftcdn.net/v2/jpg/04/41/98/07/1000_F_441980769_s5CbR4NGlv9aT9MbacDGvlpLAFbynfya.jpg"
+          />
+          <Slide
+            title="IT training"
+            desc="It's 2023. Deliver the learning experience your team deserves. And
             have a good time doing it."
-          img="https://as2.ftcdn.net/v2/jpg/04/41/98/07/1000_F_441980769_s5CbR4NGlv9aT9MbacDGvlpLAFbynfya.jpg"
-        />
-        <Slide
-          title="Online Campus preparation portal with practice sheets"
-          desc="It's 2023. Deliver the learning experience your team deserves. And
+            img="https://as2.ftcdn.net/v2/jpg/04/41/98/07/1000_F_441980769_s5CbR4NGlv9aT9MbacDGvlpLAFbynfya.jpg"
+          />
+          <Slide
+            title="Soft Skill Development Training"
+            desc="It's 2023. Deliver the learning experience your team deserves. And
             have a good time doing it."
-          img="https://as2.ftcdn.net/v2/jpg/04/41/98/07/1000_F_441980769_s5CbR4NGlv9aT9MbacDGvlpLAFbynfya.jpg"
-        /> */}
+            img="https://as2.ftcdn.net/v2/jpg/04/41/98/07/1000_F_441980769_s5CbR4NGlv9aT9MbacDGvlpLAFbynfya.jpg"
+          />
+          <Slide
+            title="IT Project"
+            desc="It's 2023. Deliver the learning experience your team deserves. And
+            have a good time doing it."
+            img="https://as2.ftcdn.net/v2/jpg/04/41/98/07/1000_F_441980769_s5CbR4NGlv9aT9MbacDGvlpLAFbynfya.jpg"
+          />
+        </Slider>
+        <div className="home-buttons">
+          <button className="home-button home-getstarted-button">
+            Get Started
+          </button>
+          <button className="home-button home-knowmore-button">
+            Know More
+          </button>
+        </div>
+      </div>
       <div className="home-hr">
         <div className="home-hr-icon">
           <FaGraduationCap size={60} fill="rgb(95, 209, 222)" />
@@ -57,37 +79,34 @@ const Slide = ({ title, desc, img }) => {
   return (
     <div className="home-slider">
       <div className="home-slider-left">
-        <div className="home-slider-social-bar">
-          <div className="home-slider-social-icon">
-            <ImFacebook size={18} fill="#040146" />
-          </div>
-          <div className="home-slider-social-icon">
-            <ImTwitter size={18} fill="#040146" />
-          </div>
-          <div className="home-slider-social-icon">
-            <BsInstagram size={18} fill="#040146" />
-          </div>
-          <div className="home-slider-social-icon">
-            <ImYoutube size={18} fill="#040146" />
-          </div>
-          <div className="home-slider-social-icon">
-            <FaLinkedinIn size={18} fill="#040146" />
-          </div>
-        </div>
         <div className="home-slider-left-details">
           <h1>{title}</h1>
           <p>{desc}</p>
-          <div className="home-buttons">
-            <button className="home-button home-getstarted-button">
-              Get Started
-            </button>
-            <button className="home-button home-knowmore-button">
-              Know More
-            </button>
-          </div>
         </div>
       </div>
       <img className="home-slider-image" src={img} alt="" />
+    </div>
+  );
+};
+
+const Socialbar = () => {
+  return (
+    <div className="home-slider-social-bar">
+      <div className="home-slider-social-icon">
+        <ImFacebook size={18} fill="#040146" />
+      </div>
+      <div className="home-slider-social-icon">
+        <ImTwitter size={18} fill="#040146" />
+      </div>
+      <div className="home-slider-social-icon">
+        <BsInstagram size={18} fill="#040146" />
+      </div>
+      <div className="home-slider-social-icon">
+        <ImYoutube size={18} fill="#040146" />
+      </div>
+      <div className="home-slider-social-icon">
+        <FaLinkedinIn size={18} fill="#040146" />
+      </div>
     </div>
   );
 };
